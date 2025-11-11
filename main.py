4020f52849a2b4cae626d39289165add90b2ffc8
Hansheng Zhang <1718810184@qq.com>
2025-11-10 16:45:15 -0800

    重命名框架类为ImageDahazingFramework，并更新主程序中的引用

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import yaml
 from src.dataset import ImageDataset
-from src.framework import ImageEnhancementFramework
+from src.framework import ImageDahazingFramework
 
 if __name__ == "__main__":
     config = yaml.safe_load(open("config.yaml", "r"))
     dataset = ImageDataset(config["data"]["input_dir"])
-    framework = ImageEnhancementFramework(config)
+    framework = ImageDahazingFramework(config)
     framework.run(dataset)
--- src/framework.py
+++ src/framework.py
@@ -2,7 +2,7 @@ import os
 from src.algorithms import clahe, dcp, ridcp
 from src.utils import save_image
 
-class ImageEnhancementFramework:
+class ImageDahazingFramework:
     def __init__(self, config):
         self.config = config
         self.methods = {
